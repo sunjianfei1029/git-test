dea763d6bee8104ff8e4fe5d02d7018809000477
sunjianfei <sunjianfei1029@126.com>
2020-11-29 17:06:22 +0800

    将MyTest添加到本地仓库

---
+++ git-test-product/src/com/bjpowernode/MyTest.java
@@ -0,0 +1,12 @@
+package com.bjpowernode;
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("git--test");
+        System.out.println("git--test");
+        System.out.println("git--test");
+        System.out.println("git--test");
+        System.out.println("git--test");
+        System.out.println("git--test");
+    }
+}
